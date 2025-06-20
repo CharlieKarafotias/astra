9206f08fa1b2afb04a57dbb64bff8d581f0be132
Charles Karafotias <cnkara2023@gmail.com>
2025-06-20 15:16:02 -0400

    windows dark mode

--- src/os_implementations/windows.rs
+++ src/os_implementations/windows.rs
@@ -1,8 +1,22 @@
+use std::error::Error;
 use std::path::PathBuf;
+use std::process::Command;
 
 // --- OS specific code ---
-pub(crate) fn is_dark_mode_active() -> bool {
-    todo!("Implement for dark mode on windows")
+pub(crate) fn is_dark_mode_active() -> Result<bool, WindowsError> {
+    let output = Command::new("powershell")
+        .arg("-Command")
+        .arg("Get-ItemPropertyValue")
+        .arg("-Path")
+        .arg("HKCU:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize")
+        .arg("-Name")
+        .arg("SystemUsesLightTheme")
+        .output()
+        .map_err(|e| WindowsError::DarkModeError(e.to_string()))?;
+    let output_str = String::from_utf8(output.stdout).map_err(|e| WindowsError::DarkModeError(e.to_string()))?;
+    let is_light_mode = output_str.trim().parse::<i8>().map_err(|e| WindowsError::DarkModeError(e.to_string()))?;
+    Ok(is_light_mode == 0)
+    
 }
 pub(crate) fn get_screen_resolution() -> (u32, u32) {
     todo!("Implement for screen resolution on windows")
@@ -21,4 +35,17 @@ pub(crate) fn path_to_desktop_folder() -> PathBuf {
 // --- Helper functions ---
 
 // --- Errors ---
+#[derive(Debug, PartialEq)]
+pub enum WindowsError {
+    DarkModeError(String),
+}
+
+impl std::fmt::Display for WindowsError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            WindowsError::DarkModeError(err) => write!(f, "Unable to determine dark mode status: {err}"),
+        }
+    }
+}
+impl Error for WindowsError {}
 // --- Errors ---
