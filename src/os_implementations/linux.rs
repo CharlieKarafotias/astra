961aa2a22c041b17ab660d71f5fb0466db2c89a4
Charlie Karafotias <cnkara2023@gmail.com>
2025-07-08 07:58:10 -0400

    finished linux dark mode setup

--- src/os_implementations/linux.rs
+++ src/os_implementations/linux.rs
@@ -1,7 +1,22 @@
-use std::path::PathBuf;
+use std::{fmt::Display, path::PathBuf, process::Command};
 // --- OS specific code ---
-pub(crate) fn is_dark_mode_active() -> bool {
-    todo!("Implement for dark mode on linux")
+/// CHecks if the user's OS is currently in dark mode
+///
+/// Tested on:
+///   - Ubuntu 25.04 with Gnome Desktop
+pub(crate) fn is_dark_mode_active() -> Result<bool, LinuxOSError> {
+    let output = Command::new("gsettings")
+        .arg("get")
+        .arg("org.gnome.desktop.interface")
+        .arg("color-scheme")
+        .output()
+        .map_err(|e| LinuxOSError::DarkModeError(e.to_string()))?;
+    let output_str = String::from_utf8_lossy(&output.stdout);
+    let dark_mode_enabled = match output_str.trim().to_lowercase().as_str() {
+        "'prefer-dark'" => true,
+        _ => false,
+    };
+    Ok(dark_mode_enabled)
 }
 pub(crate) fn get_screen_resolution() -> (u32, u32) {
     todo!("Implement for screen resolution on linux")
@@ -20,4 +35,28 @@ pub(crate) fn path_to_desktop_folder() -> PathBuf {
 // --- Helper functions ---
 
 // --- Errors ---
+#[derive(Debug, PartialEq)]
+pub enum LinuxOSError {
+    DarkModeError(String),
+    HomeEnvVarNotFound,
+    MainDisplayNotFound,
+    ResolutionNotFound,
+}
+
+impl Display for LinuxOSError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            LinuxOSError::DarkModeError(err_msg) => {
+                write!(f, "Unable to determine dark mode status: {err_msg}")
+            }
+            LinuxOSError::HomeEnvVarNotFound => {
+                write!(f, "Unable to find $HOME environment variable")
+            }
+            LinuxOSError::MainDisplayNotFound => write!(f, "Unable to determine main display"),
+            LinuxOSError::ResolutionNotFound => {
+                write!(f, "Unable to determine resolution of main display")
+            }
+        }
+    }
+}
 // --- Errors ---
